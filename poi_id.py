aad106339635a1e4efb846d983c485cc640edfef
AndreyN <ahahac@yandex.ru>
2016-05-22 14:38:59 +0100

    Final code.

--- poi_id.py
+++ poi_id.py
@@ -22,26 +22,12 @@ from tester import dump_classifier_and_data
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
-features_list = ['poi', 'salary', 'bonus', 'total_payments', 'exercised_stock_options', 'director_fees'] # You will need to use more features
+features_list = ['poi', 'salary', 'bonus', 'total_payments', 'exercised_stock_options'] # You will need to use more features
 		#'shared_receipt_with_poi',
 		#, 'to_messages',
+		#, 'total_stock_value' -- Made recall worse!
 		#'from_poi_to_this_person', 'from_this_person_to_poi'
-
-print features_list
-
-### Load the dictionary containing the dataset
-with open("final_project_dataset.pkl", "r") as data_file:
-    data_dict = pickle.load(data_file)
-
-print "Total data points: ", len(data_dict)
-
-### Task 2: Remove outliers
-#Remove "TOTAL" line
-data_dict.pop( "TOTAL", 0 )
-print "Data points after outliers removal:", len(data_dict)
-
-### Task 3: Create new feature(s)
-### Store to my_dataset for easy export below.
+		#, 'expenses'
 
 #define a function to convert string to float.
 def num(s):
@@ -54,34 +40,55 @@ def num(s):
     except ValueError:
         return 0.0
 
-my_dataset = data_dict
-poi_count = 0
-#Let create a new feature: ratio of salary and total payments
-#for x in my_dataset:
-#	if math.isnan(num(my_dataset[x]['total_payments'])):
-#		my_dataset[x]['ratio_sal_totalp'] = 0.0
-#	else:
-#		if num(my_dataset[x]['total_payments']) == 0.0:
-#			my_dataset[x]['ratio_sal_totalp'] = 0.0
-#		else:
-#			my_dataset[x]['ratio_sal_totalp'] = num(my_dataset[x]['salary'])/num(my_dataset[x]['total_payments'])
-#	if math.isnan(num(my_dataset[x]['to_messages'])) | (num(my_dataset[x]['to_messages']) == 0.0):
-#		my_dataset[x]['to2from_poi_to_this_person'] = 0.0
-#	else:
-#		my_dataset[x]['to2from_poi_to_this_person'] = num(my_dataset[x]['from_poi_to_this_person'])/num(my_dataset[x]['to_messages'])
-#	if math.isnan(num(my_dataset[x]['from_messages'])) | (num(my_dataset[x]['from_messages']) == 0.0):
-#		my_dataset[x]['from2from_this_person_to_poi'] = 0.0
-#	else:
-#		my_dataset[x]['from2from_this_person_to_poi'] = num(my_dataset[x]['from_this_person_to_poi'])/num(my_dataset[x]['from_messages'])
+### Load the dictionary containing the dataset
+with open("final_project_dataset.pkl", "r") as data_file:
+    data_dict = pickle.load(data_file)
 
+print "Number of data points: ", len(data_dict)
+
+### Task 2: Remove outliers
+#Remove "TOTAL" line
+data_dict.pop( "TOTAL", 0 )
+data_dict.pop( "THE TRAVEL AGENCY IN THE PARK", 0 )
+print "Data points after outliers removal:", len(data_dict)
+
+## Let's produce some stats on the data set
+dict_nan_counts = {}
+dict_nan_pp_counts = {}
+for key1 in data_dict:
+	dict_nan_pp_counts[key1] = 0
+	for key2 in data_dict[key1]:
+		#print key2
+		if data_dict[key1][key2] == 'NaN':
+			dict_nan_pp_counts[key1] = dict_nan_pp_counts[key1] + 1
+			if key2 in dict_nan_counts:
+				dict_nan_counts[key2] = dict_nan_counts[key2] + 1
+			else:
+				dict_nan_counts[key2] = 1
+	
+print "Features NaN counts: ", dict_nan_counts
+print "Biggest number of NaNs per person:"
+for key1 in dict_nan_pp_counts:
+	if dict_nan_pp_counts[key1] > 15:
+		print key1, ' ', dict_nan_pp_counts[key1]
+#print dict_nan_pp_counts
+
+### Task 3: Create new feature(s)
+### Store to my_dataset for easy export below.
+
+my_dataset = data_dict
+
+##Let create a new feature: ratio of exercised_stock_options to total payments/total stocks
+## Ration of from_poi_to_this_person to to_messages
+## Ration of from_this_person_to_poi to from_messages
 for x in my_dataset:
 	if math.isnan(num(my_dataset[x]['total_payments'])):
-		my_dataset[x]['ratio_exso_totalp'] = 0.0
+		my_dataset[x]['ratio_bonus_totalp'] = 0.0
 	else:
 		if num(my_dataset[x]['total_payments']) == 0.0:
-			my_dataset[x]['ratio_exso_totalp'] = 0.0
+			my_dataset[x]['ratio_bonus_totalp'] = 0.0
 		else:
-			my_dataset[x]['ratio_exso_totalp'] = num(my_dataset[x]['exercised_stock_options'])/num(my_dataset[x]['total_payments'])
+			my_dataset[x]['ratio_bonus_totalp'] = num(my_dataset[x]['bonus'])/num(my_dataset[x]['total_payments'])
 	if math.isnan(num(my_dataset[x]['to_messages'])) | (num(my_dataset[x]['to_messages']) == 0.0):
 		my_dataset[x]['to2from_poi_to_this_person'] = 0.0
 	else:
@@ -90,28 +97,41 @@ for x in my_dataset:
 		my_dataset[x]['from2from_this_person_to_poi'] = 0.0
 	else:
 		my_dataset[x]['from2from_this_person_to_poi'] = num(my_dataset[x]['from_this_person_to_poi'])/num(my_dataset[x]['from_messages'])
-
-#features_list.append("ratio_sal_totalp")
-#features_list.append("ratio_bonus_totalp")
-features_list.append("ratio_exso_totalp")
+		
+	if math.isnan(num(my_dataset[x]['total_stock_value'])):
+		my_dataset[x]['ratio_exso_totals'] = 0.0
+	else:
+		if num(my_dataset[x]['total_stock_value']) == 0.0:
+			my_dataset[x]['ratio_exso_totals'] = 0.0
+		else:
+			my_dataset[x]['ratio_exso_totals'] = num(my_dataset[x]['exercised_stock_options'])/num(my_dataset[x]['total_stock_value'])
+	
+features_list.append("ratio_bonus_totalp")
+features_list.append("ratio_exso_totals")
 features_list.append("to2from_poi_to_this_person")
 features_list.append("from2from_this_person_to_poi")
 print "Final features list:", features_list
+
 ### Extract features and labels from dataset for local testing
-data = featureFormat(my_dataset, features_list, sort_keys = False) #17/05/2016 changed sort_keys from True to False
+data = featureFormat(my_dataset, features_list, sort_keys = True)
 labels, features = targetFeatureSplit(data)
+
+## The below is an attempt to use SelectKBest.
+## Effect is the same as just using PCA.
+#from sklearn.feature_selection import SelectKBest
+#from sklearn.feature_selection import chi2
+#clf = SelectKBest(chi2, k=2).fit(features, labels)
+#features_new = clf.transform(features)
+#print "Scores: ", clf.scores_
+#print "Features new shape: ", features_new.shape
+#print "Features new: ", features_new
+#features = features_new
 
 count_poi = 0
 count_nonpoi = 0
 count_all = len(data)
 
-print "Count all after feature split:", count_all
-print "-----------3-------------"
-print "0:",data[0]
-print "1:",data[1]
-print "2:",data[2]
-print "3:",data[3]
-print "-----------4-------------"
+print "All: ", count_all
 
 for point in data:
     salary = point[1]
@@ -176,10 +196,11 @@ from sklearn.neighbors import KNeighborsClassifier
 #estimators = [('reduce_dim', PCA()), ('decisiontree', DecisionTreeClassifier(random_state=0, min_samples_leaf=1))] # precision 0.29493 recall 0.30275
 #estimators = [('reduce_dim', PCA()), ('decisiontree', DecisionTreeClassifier(random_state=0, min_samples_leaf=1, max_features='auto'))] # precision 0.27664 recall 0.28950
 #estimators = [('reduce_dim', PCA(n_components=4)), ('classifier', RandomForestClassifier(n_estimators=120, min_samples_leaf=2))] # Accuracy: 0.85760       Precision: 0.43573      Recall: 0.23050 F1: 0.30150     F2: 0.25447
-#estimators = [('reduce_dim', PCA(n_components=3)), ('classifier', SVC())] # Precision or recall may be undefined due to a lack of true positive predicitons.
+#estimators = [('reduce_dim', PCA(n_components=3)), ('classifier', SVC(kernel='rbf'))] # Precision or recall may be undefined due to a lack of true positive predicitons.
 #estimators = [('reduce_dim', PCA(n_components=4)), ('classifier', GaussianNB())] # Accuracy: 0.85953       Precision: 0.45668      Recall: 0.28200 F1: 0.34869     F2: 0.30536
 #estimators = [('reduce_dim', PCA(n_components=4)), ('classifier', AdaBoostClassifier(n_estimators=100))] #Accuracy: 0.83440       Precision: 0.35765      Recall: 0.30400 F1: 0.32865     F2: 0.31340
-estimators = [('reduce_dim', PCA(n_components=3)), ('classifier', KNeighborsClassifier(n_neighbors=3, algorithm='kd_tree'))] #Accuracy: 0.87220       Precision: 0.53496      Recall: 0.31750 F1: 0.39849     F2: 0.34560
+#estimators = [('reduce_dim', PCA(n_components=3)), ('classifier', KNeighborsClassifier(n_neighbors=3))] #Accuracy: 0.87220       Precision: 0.53496      Recall: 0.31750 F1: 0.39849     F2: 0.34560
+estimators = [('reduce_dim', PCA(n_components=3)), ('classifier', KNeighborsClassifier(n_neighbors=3, p=1))] #Accuracy: 0.88073       Precision: 0.58669      Recall: 0.35700 F1: 0.44389     F2: 0.38733
 
 clf = Pipeline(estimators)
 
@@ -197,17 +218,19 @@ features_train, features_test, labels_train, labels_test = \
 
 from sklearn.cross_validation import cross_val_score
 
-clf.fit(features_train, labels_train)
+#clf.fit(features_train, labels_train)
 
 #dt = clf.named_steps['decisiontree']
 #print "Feature importances:", dt.feature_importances_
 
-pca = clf.named_steps['reduce_dim']
-print("PCA Explained: ", pca.explained_variance_ratio_) 
-print pca.components_ 
+#pca = clf.named_steps['reduce_dim']
+#print("PCA Explained: ", pca.explained_variance_ratio_) 
+#print pca.components_ 
 
-scores = cross_val_score(clf, features_test, labels_test)
+scores = cross_val_score(clf, features, labels)
 print "Cross-validation scores mean: ", scores.mean()
+
+clf.fit(features_train, labels_train)
 print "Classifier score: ", clf.score(features_test, labels_test)
 
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
